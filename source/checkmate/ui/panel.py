204f5bcd145f1112ce3dd277e8e9df5a1677075b
AM Studio <amstudionetwork@gmail.com>
2026-07-28 17:39:29 +0530

    Add validation result

--- source/checkmate/ui/panel.py
+++ source/checkmate/ui/panel.py
@@ -14,17 +14,19 @@ class CHECKMATE_PT_MainPanel(bpy.types.Panel):
         layout = self.layout
 
         box = layout.box()
-
         box.label(text="Health Score", icon="INFO")
-
         box.label(text="--")
 
         status_box = layout.box()
-
         status_box.label(text="Project Status", icon="CHECKMARK")
-
         status_box.label(text="Not Scanned")
 
         layout.separator()
 
         layout.operator("checkmate.run_scan", icon="PLAY")
+
+        layout.separator()
+
+        results_box = layout.box()
+        results_box.label(text="Validation Results", icon="TEXT")
+        results_box.label(text="No scan performed.")
